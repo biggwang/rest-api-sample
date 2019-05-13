02b48c4cfbef290ad65303f8e68fe526a716059f
Yungwang <boris_winter@naver.com>
2019-05-13 23:00:30 +0900

    first commit

---
+++ src/main/java/com/ryu/rest/WhiteshipRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.ryu.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteshipRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteshipRestApiApplication.class, args);
+	}
+
+}
